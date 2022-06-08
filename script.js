019873bb4c39977c49ac2df0dec83236b70a8357
audriusxp <palm@inbox.lt>
2022-06-08 19:43:53 +0300

    atlikta pirma dalis

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-    
+    let zodis = "Čia yra kažkoks tekstas";
+    let raide = zodis[0];
+    console.log(raide)
+    console.log(zodis)
+   
+
+   
+   
+}
+function raide (a,)
+ {
+    raide = zodis(a)
+    return zodis;
+
+
 }
