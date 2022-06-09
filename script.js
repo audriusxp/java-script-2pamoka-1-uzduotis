28dd1ed3111a0377cdf9799acced9a5aa47ea0a4
audriusxp <palm@inbox.lt>
2022-06-09 14:07:41 +0300

    pabaigta uzduotis

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 window.onload = function() {
     let zodis = "Čia yra kažkoks tekstas";
     let raide = zodis[0];
+    let raide1= zodis[4];
+    let raide2 = zodis[10];
+    
     console.log(raide)
     console.log(zodis)
+    console.log(raide1)
+    console.log(raide2)
+   
    
 
    
    
 }
-function raide (a,)
+function raide (a)
+
  {
     raide = zodis(a)
+   
     return zodis;
 
 
